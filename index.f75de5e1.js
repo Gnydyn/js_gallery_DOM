4621eecf774600e3a4e14a0f0e3eb8c157891de5
Liudmyla Hnydyn <gnydynluda@gmail.com>
2024-08-17 17:35:56 -0400

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,10 @@
+"use strict";
+const gallery = document.querySelector(".gallery");
+const largeImage = document.getElementById("largeImg");
+gallery.addEventListener("click", function(ev) {
+    ev.preventDefault();
+    const galleryImg = ev.target.closest(".list-item__link");
+    largeImage.src = galleryImg.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
